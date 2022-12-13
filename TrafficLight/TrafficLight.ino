1d683671c425db6c5eab4403b5bc4c04a79d90b6
孟凡钧 <42081913+MengFanjun020906@users.noreply.github.com>
2022-12-13 18:48:38 +0800

    Add files via upload

---
+++ TrafficLight/TrafficLight.ino
@@ -0,0 +1,40 @@
+#define red 1
+#define yellow 2
+#define green 3
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+}
+void loop() {
+    trafficLights(red);
+    trafficLights(yellow);
+    trafficLights(green);
+}
+void trafficLights(int light)
+{
+  switch(light){
+  case 1:    digitalWrite(2,HIGH);
+             digitalWrite(3,LOW);
+             digitalWrite(4,LOW);
+             delay(4000);
+  case 2:
+              for(int i=0;i<7;i++)
+              {
+              digitalWrite(2,LOW);
+              digitalWrite(3,LOW);  
+              digitalWrite(4,HIGH);
+              delay(200);
+              digitalWrite(2,LOW);
+              digitalWrite(3,LOW);  
+              digitalWrite(4,LOW);
+              delay(200);
+              }  
+  case 3:    digitalWrite(2,LOW);
+             digitalWrite(3,HIGH);
+             digitalWrite(4,LOW);
+             delay(4000);
+  break;
+  }
+}
+
